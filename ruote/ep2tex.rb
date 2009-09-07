e1fc60ba7d625011c431b12fdda9f1817663d783
John Mettraux <jmettraux@gmail.com>
2009-09-07 22:53:57 +0900

    integrated latest expression documentation

--- ruote/ep2tex.rb
+++ ruote/ep2tex.rb
@@ -9,16 +9,16 @@ FileUtils.mkdir('tex') rescue nil
 
 paths = []
 
-src = File.expand_path(File.join(*%w[ ~ ruota lib ruote exp ]))
+src = File.expand_path(File.join(*%w[ ~ w ruota lib ruote exp ]))
 paths = paths + Dir.glob(File.join(src, "fe_*.rb"))
 
-src = File.expand_path(File.join(*%w[ ~ ruota lib ruote part ]))
+src = File.expand_path(File.join(*%w[ ~ w ruota lib ruote part ]))
 paths = paths + Dir.glob(File.join(src, "*_participant.rb"))
 
-src = File.expand_path(File.join(*%w[ ~ ruote-dm lib ruote dm part ]))
+src = File.expand_path(File.join(*%w[ ~ w ruote-dm lib ruote dm part ]))
 paths = paths + Dir.glob(File.join(src, "*_participant.rb"))
 
-src = File.expand_path(File.join(*%w[ ~ ruote-amqp lib ])) # :(
+src = File.expand_path(File.join(*%w[ ~ w ruote-amqp lib ])) # :(
 paths = paths + Dir.glob(File.join(src, "*_participant.rb"))
 
 
